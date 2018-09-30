d917daf13cc6111b2dbd89177236f3140374aeef
Jose Vinicius <josevini29@gmail.com>
2018-09-30 13:48:32 -0300

    Teste postgres

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+#print('Erro ao tentar encontrar variavel de ambiente do banco de dados.')
 
 @app.route("/")
 def hello():
     return '{"status":"ok","message":"hello word!","by":"José Vinicius","email":"josevini29@hotmail.com","Country":"Brazil"}', 200
 
 if __name__ == "__main__":
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+  
